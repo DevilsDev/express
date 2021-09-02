125e6e1884c8eedf30fd7abda15b81ba37aef6a8
Ali Kahwaji <alikahwaji@alis-mbp.lan>
2021-09-03 06:11:37 +1200

    Pushing node work

--- _node/app.js
+++ _node/app.js
@@ -1,11 +1,11 @@
-const  express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send("Hello world!");
+app.get("/", (req, res) => {
+	res.send("Hello world!");
 });
 
 app.listen(3000, () => {
-  console.log('started on port 3000.');
+	console.log("started on port 3000.");
 });
