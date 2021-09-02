639bba43652444ab73e47d52fd619364e5cb3859
alikahwaji <ali.kahwaji@hotmail.com>
2021-09-03 07:19:21 +1200

    Completed API code

---
+++ _express/_APIs/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/random/:min/:max", (req, res) => {
+	const min = parseInt(req.params.min);
+	const max = parseInt(req.params.max);
+	if (isNaN(min) || isNaN(max)) {
+		res.status(400);
+		res.json({ error: "Bad request." });
+		return;
+	}
+
+	const result = Math.round(Math.random() * (max - min) + min);
+
+	res.json({ result: result });
+});
+
+app.listen(3000, () => {
+	console.log("App started on port 3000");
+});
